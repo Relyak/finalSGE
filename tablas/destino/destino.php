2bf01027beb2b6d9da49b4771c150e2f81aef398
ronchassssss <kaylerdavid@gmail.com>
2023-02-17 16:43:10 +0100

    titulo en conductores

--- tablas/destino/destino.php
+++ tablas/destino/destino.php
@@ -2,24 +2,20 @@
     //<img src="recursos/tablaDestino.jpg" alt="">
     //<img src="recursos/tablaSus.jpg" alt="">
     //<img src="recursos/tablaCamionero.png" alt="">
-    $stmt = $conn->prepare("SELECT * FROM conductor");
+    $stmt = $conn->prepare("SELECT * FROM destino");
     $stmt->execute();
 
     $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
     //var_dump($result);
     //echo '<a href="newGolfer.php"><h1>CREAR GOLFISTA</h1></a></br></br>';
-    echo '<h3>Conductores</h3>';
+    echo '<h3>Destino</h3>';
     echo '<table width="100px" class="table table-striped">';
-    echo '<tr><th>Id</th><th>Nombre</th><th>Apellidos</th><th>Dni</th><th>Matrícula</th><th>Teléfono</th><th>Email</th><th>Nacionalidad</th><th><a href=tablas/conductores/crud/PagCrear.php>Añadir</a></th><th></th></tr>';
+    echo '<tr><th>Id</th><th>CIF</th><th>Nombre</th><th>Dirección</th><a href=tablas/conductores/crud/PagCrear.php>Añadir</a></th><th></th></tr>';
     foreach ($result as $row) {
         echo "<tr><td>" . $row["id"] . "</td>"
-            . "<td>" . $row["nombre"] . "</td>" 
-            . "<td>" . $row["apellidos"] . "</td>"
-            . "<td>" . $row["dni"] . "</td>"
-            . "<td>" . $row["matricula"] . "</td>"
-            . "<td>" . $row["telefono"] . "</td>"
-            . "<td>" . $row["email"] . "</td>"
-            . "<td>" . $row["nacionalidad"] . "</td>"
+            . "<td>" . $row["cif"] . "</td>" 
+            . "<td>" . $row["nombre"] . "</td>"
+            . "<td>" . $row["direccion"] . "</td>"
             . "<td>" . "<a href='tablas/conductores/crud/borrarConductor.php?id=".$row["id"]."'>Borrar</a>" . "</td>"
             . "<td>" . "<a href='tablas/conductores/crud/PagEditar.php?id=".$row["id"]."'>Editar</a>" . "</td></tr>";
         // . $row["apellidos"]. " - "; 
